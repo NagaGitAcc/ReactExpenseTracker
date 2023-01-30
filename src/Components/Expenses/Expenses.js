dcf591f0e242001fcb84e3074d0dda6183912109
agiratech-nagarajan <nagarajan.s@agiratech.com>
2023-01-30 18:58:56 +0530

    Added Conditional Rendering

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
+import ExpensesList from './ExpensesList';
 
 const Expenses = props => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -14,15 +14,14 @@ const Expenses = props => {
 
   const filteredExpenses = props.items.filter(expense => expense.date.getFullYear().toString() === filteredYear);
 
+
   return (
-    <div>
+    <li>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {filteredExpenses.map(expense => (
-          <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
-        ))}
+        <ExpensesList items={filteredExpenses} />
       </Card>
-    </div>
+    </li>
   );
 };
 
--- src/Components/NewExpense/NewExpense.js
+++ src/Components/NewExpense/NewExpense.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
-const NewExpense = (props) => {
-  const saveExpenseDataHandler = (enteredExpenseData) => {
+const NewExpense = props => {
+  const [addExpenseFlag, setAddExpenseFlag] = useState(true);
+
+  const saveExpenseDataHandler = enteredExpenseData => {
     const expenseData = {
       ...enteredExpenseData,
-      id: Math.random().toString()
+      id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
   };
 
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {addExpenseFlag && <button onClick={() => setAddExpenseFlag(false)}>Add New Expense</button>}
+      {!addExpenseFlag && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} expenseFlag={setAddExpenseFlag} />}
     </div>
   );
 };
